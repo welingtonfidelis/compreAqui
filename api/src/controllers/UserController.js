34cb0ece6c23bc6c2125f6c8abfaf7d07f8d6df8
welington <welingtonfidelis@gmail.com>
2020-04-21 13:20:07 -0300

    Cadastro de usuráio por api finalizado

--- api/src/controllers/UserController.js
+++ api/src/controllers/UserController.js
@@ -1,21 +1,30 @@
-const { User } = require('../models');
+const { User, Address } = require('../models');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 module.exports = {
     async store(req, res) {
         try {
             const { 
-                name, doc, email, phone1, phone2, user, 
-                birth, password, passwordConfirm, type, cep, state, 
+                name, doc, email, phone1, phone2, user, number, 
+                birth, type, cep, state, 
                 city, district, street, complement
             } = req.body;
-            
+
+            const password = bcrypt.hashSync(req.body.password, saltRounds);
+
             let photoUrl = null;
             if(req.file) photoUrl = req.file.location;
 
-            const query = await User.create({
-                name, doc, email, phone1, phone2, user, 
-                birth, password, passwordConfirm, type, cep, state, 
-                city, district, street, complement, photoUrl
+            let query = await Address.create({
+                cep, state, city, district,
+                street, complement, number,
+            });
+            const AddressId = query.dataValues.id;
+
+            query = await User.create({
+                name, doc, email, phone1, phone2, user, photoUrl,
+                birth, password, type, AddressId
             });
             
             res.status(200).send({ status: true, response: query, code: 20 });
